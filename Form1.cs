7472eaf923970a4dc33773edd4fefdd9aafb5713
Sovik83 <sovik_channel@mail.ru>
2022-03-11 20:56:24 +0200

    1LB
    1LB

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+namespace LB1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = textBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = textBox1.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Text = textBox1.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Text = textBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
